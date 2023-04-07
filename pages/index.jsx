b1360ad5eaf20ca82686a030476cb16699c8270a
Berzan Özejder <berzanozejder@gmail.com>
2023-04-07 11:35:52 +0300

    win

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,50 @@
-import { Button } from "@chakra-ui/react";
+import { Avatar, Button } from "@chakra-ui/react";
 import Image from "next/image";
 import { Fragment } from "react";
 import style from "@/styles/index.module.css";
 import { useRouter } from "next/router";
 import { Spacer } from "@nextui-org/react";
 import Head from "next/head";
+import { Text } from "@chakra-ui/react";
 
 export default function page() {
+	function winner(name, image, position) {
+		return (
+			<div className={style.winner}>
+				<div style={{ backgroundImage: `url(/attributes/candidates/${image}.jpg)` }} className={style.left}></div>
+				<div className={style.right}>
+					<Text fontSize="20px" fontWeight="400">
+						{position}
+					</Text>
+					<Text fontSize="20px" fontWeight="600">
+						{name}
+					</Text>
+				</div>
+			</div>
+		);
+	}
+
 	const router = useRouter();
 	return (
-		<div className={style.page}>
+		<Fragment>
 			<Head>
 				<title>ES Student Elections</title>
 			</Head>
-			<div className={style.everyone}>
-				<Image alt="Photograph of all candidates together" src="/attributes/images/everyone.jpg" width={2048} height={1536} />
+			<div className={style.page}>
+				<div className={style.winners}>
+					<Text fontSize="20px" fontWeight="600">
+						Winners
+					</Text>
+					{winner("Marisa Troullidou", "marisa", "Head Girl")}
+					{winner("Philemon Antoniou", "philemon", "Head Boy")}
+					{winner("Louissa Maria Maroudia", "louissa", "Deputy Head Girl")}
+					{winner("George Alexander Tornaritis", "george", "Deputy Head Boy")}
+
+					<Button onClick={() => router.push("/candidates")} colorScheme="blue">
+						See All Candidates 🚀
+					</Button>
+				</div>
 			</div>
-			<Spacer y={1} />
-			<Button onClick={() => router.push("/candidates")} colorScheme="blue">
-				See Candidates 🚀
-			</Button>
-		</div>
+		</Fragment>
 	);
 }
